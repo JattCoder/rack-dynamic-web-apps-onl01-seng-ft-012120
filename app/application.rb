262bdf472d5b65d5a1a53d7895142dea5f4572af
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-02-23 18:00:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Application
 
   def call(env)
@@ -19,25 +18,3 @@ class Application
   end
 
 end
-=======
-class Application
-
-  def call(env)
-    resp = Rack::Response.new
-    num1 = Kernel.rand(1...20)
-    num2 = Kernel.rand(1...20)
-    num3 = Kernel.rand(1...20)
-    resp.write "Let's see if you have won or no.\n"
-    resp.write "First Number: #{num1}\n"
-    resp.write "Second Number: #{num2}\n"
-    resp.write "Third Number: #{num3}\n"
-    if num1 == num2 && num2 == num3
-      resp.write "You Win"
-    else
-      resp.write "You Lose"
-    end
-    resp.finish
-  end
-
-end
->>>>>>> 2582d10c9c450686d320b6ca593441bfbbc9ec19
